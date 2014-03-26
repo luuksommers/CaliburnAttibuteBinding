a8eb9bed1c96c59516814b2a69bf10e1a7d5e724
Luuk Sommers <luuk.sommers@gmail.com>
2014-03-26 21:47:56 +0100

    Add check if attribute is present

--- src/WpfApplication1/Bootstrapper.cs
+++ src/WpfApplication1/Bootstrapper.cs
@@ -1,18 +1,15 @@
-﻿using System.Windows.Controls;
-using Caliburn.Micro;
-using System;
-using System.Collections.Generic;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
+using Caliburn.Micro;
 using WpfApplication1.Converters;
 using Xceed.Wpf.Toolkit;
 
 namespace WpfApplication1
 {
-    public class MyBootstrapper : Bootstrapper<ShellViewModel> 
+    public class MyBootstrapper : Bootstrapper<ShellViewModel>
     {
         private static readonly IValueConverter RangeMaximumConverter = new RangeMaximumConverter();
         private static readonly IValueConverter RangeMinimumConverter = new RangeMinimumConverter();
@@ -28,34 +25,39 @@ namespace WpfApplication1
                     if (!ConventionManager.SetBindingWithoutBindingOrValueOverwrite(viewModelType, path, property, element, convention, DecimalUpDown.ValueProperty))
                         return false;
 
-                    if (ConventionManager.HasBinding(element, DecimalUpDown.MaximumProperty)) 
-                        return true;
+                    if (property.GetCustomAttributes(typeof(RangeAttribute), true).Any())
+                    {
+                        if (!ConventionManager.HasBinding(element, DecimalUpDown.MaximumProperty))
+                        {
+                            var binding = new Binding(path) { Mode = BindingMode.OneTime, Converter = RangeMaximumConverter, ConverterParameter = property };
+                            BindingOperations.SetBinding(element, DecimalUpDown.MaximumProperty, binding);
+                        }
 
-                    var binding = new Binding(path) { Mode = BindingMode.OneTime, Converter = RangeMaximumConverter, ConverterParameter = property };
-                    BindingOperations.SetBinding(element, DecimalUpDown.MaximumProperty, binding);
-
-                    if (ConventionManager.HasBinding(element, DecimalUpDown.MinimumProperty))
-                        return true;
-
-                    binding = new Binding(path) { Mode = BindingMode.OneTime, Converter = RangeMinimumConverter, ConverterParameter = property };
-                    BindingOperations.SetBinding(element, DecimalUpDown.MinimumProperty, binding);
+                        if (!ConventionManager.HasBinding(element, DecimalUpDown.MinimumProperty))
+                        {
+                            var binding = new Binding(path) { Mode = BindingMode.OneTime, Converter = RangeMinimumConverter, ConverterParameter = property };
+                            BindingOperations.SetBinding(element, DecimalUpDown.MinimumProperty, binding);
+                        }
+                    }
 
                     return true;
                 };
 
-            var originalBinding = ConventionManager.GetElementConvention(typeof (TextBox)).ApplyBinding;
+            var originalBinding = ConventionManager.GetElementConvention(typeof(TextBox)).ApplyBinding;
             ConventionManager.GetElementConvention(typeof(TextBox)).ApplyBinding =
                 (viewModelType, path, property, element, convention) =>
                 {
                     if (!originalBinding(viewModelType, path, property, element, convention))
                         return false;
 
-                    if (ConventionManager.HasBinding(element, TextBox.MaxLengthProperty))
-                        return true;
-
-                    var binding = new Binding(path) { Mode = BindingMode.OneTime, Converter = MaxLengthConverter, ConverterParameter = property };
-                    BindingOperations.SetBinding(element, TextBox.MaxLengthProperty, binding);
-
+                    if (property.GetCustomAttributes(typeof(StringLengthAttribute), true).Any())
+                    {
+                        if (!ConventionManager.HasBinding(element, TextBox.MaxLengthProperty))
+                        {
+                            var binding = new Binding(path) { Mode = BindingMode.OneTime, Converter = MaxLengthConverter, ConverterParameter = property };
+                            BindingOperations.SetBinding(element, TextBox.MaxLengthProperty, binding);
+                        }
+                    }
                     return true;
                 };
         }
